99346f33b1cc992647acc1af4172c8cc0f846272
mhaoufhine <adejeru1@gmail.com>
2023-03-22 15:01:19 -0700

    the first assignment was to write a program to print

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,48 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - prints the first 98 fibonacci numbers
- * starting with 1 and 2, followed by a new line.
- *
- * Return: Always 0 (success)
+ * main -  program that finds and prints the first 98 Fibonacci numbers
+ * Return: 0 (success)
  */
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum, first, second;
+	unsigned long fib1_first, fib1_second, fib2_first, fib2_second;
 
-	j = 1;
-	k = 2;
-
-	printf("%lu", j);
-
-	for (i = 1; i < 91; i++)
+	for (count = 0; count < 92; count++)
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
 	}
 
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
+	fib1_first = fib1 / 10000000000;
+	fib2_first = fib2 / 10000000000;
+	fib1_second = fib1 % 10000000000;
+	fib2_second = fib2 % 10000000000;
 
-	for (i = 92; i < 99; ++i)
+	for (count = 93; count < 99; count++)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
-	}
+		first = fib1_first + fib2_first;
+		second = fib1_second + fib2_second;
 
+		if (fib1_second + fib2_second > 9999999999)
+		{
+			first += 1;
+			second %= 10000000000;
+		}
+
+		printf("%lu%lu", first, second);
+		if (count != 98)
+			printf(", ");
+
+		fib1_first = fib2_first;
+		fib1_second = fib2_second;
+		fib2_first = first;
+		fib2_second = second;
+	}
 	printf("\n");
-
 	return (0);
 }
