64707204f9a1a56c728063840afae34d92193e4d
mhaoufhine <adejeru1@gmail.com>
2023-04-12 16:49:47 -0700

    Great Personality

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,36 +10,81 @@
  * Return: number of words
  */
 
-char **strtow(char *str)
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
 {
-	int i, flag, len;
-	char **words;
+	int flag, c, w;
 
-	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
-		return (NULL);
+	flag = 0;
+	w = 0;
 
-	i = flag = len = 0;
-	while (str[i])
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (flag == 0 && str[i] != ' ')
-			flag = 1;
-		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-		{
+		if (s[c] == ' ')
 			flag = 0;
-			len++;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
 		}
-		i++;
 	}
 
-	len += flag == 1 ? 1 : 0;
-	if (len == 0)
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
 		return (NULL);
 
-	words = (char **)malloc(sizeof(char *) * (len + 1));
-	if (words == NULL)
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
 
-	util(words, str);
-	words[len] = NULL;
-	return (words);
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
