a3ff90fdd01dc6b64ff71af33b51ccb972369822
mhaoufhine <adejeru1@gmail.com>
2023-04-17 14:57:40 -0700

    Outside of a dog is the owner lol

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,14 +3,14 @@
 
 
 /**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
 	{
 		char *name;
 		float age;
@@ -32,4 +32,4 @@
 	int _strlen(char *s);
 
 
-	#endif
+#endif
